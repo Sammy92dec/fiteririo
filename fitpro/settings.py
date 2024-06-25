26304779f8a635667d177f1f3b2b6a2034b981d4
Samrawit Tekheste <samrawit.samuel.tk@gmail.com>
2024-06-25 09:54:59 +0000

    Create a database in ElephantSQL and connect it with the workplace

--- fitpro/settings.py
+++ fitpro/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(z#wyz6l4ok8$9otq2xbd)5ov2xl*snhe7!h0l*9&xen!tlm5n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-sammy92dec-fiteririo-x5foksff8lf.ws.codeinstitute-ide.net']
 
@@ -39,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'fit_app',
     'about',
-    'member_app/',
+    'member_app',
 ]
 
 MIDDLEWARE = [
@@ -76,11 +80,15 @@ WSGI_APPLICATION = 'fitpro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("postgres://wwxilrho:OSO8uANzz7IElS4a31UUiVeqwXpMptWB@abul.db.elephantsql.com/wwxilrho"))
 }
 
 
