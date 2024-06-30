bd983e7f8c9db6d053c7e17fe961d20a2c21aa4b
Samrawit Tekheste <samrawit.samuel.tk@gmail.com>
2024-06-30 00:25:47 +0000

    Add DOB and define to create and update user profile

--- member_app/models.py
+++ member_app/models.py
@@ -10,6 +10,12 @@ COMPETENCE_TYPES = (
     ('advance','Advance'),
 )
 
+GENDER = (
+    ('male', 'Male'),
+    ('female', 'Female'),
+    ('other', 'Other'),
+    )
+
 
 class Member(models.Model):
     """
@@ -19,30 +25,25 @@ class Member(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image_alt = models.URLField(blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=GENDER, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     city = models.CharField(max_length=40, null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
     slug = models.SlugField(max_length=200, unique=True)
-    competence_types = models.CharField(max_length=50, choices=COMPETENCE_TYPES, default='beginner')
+    competence_types = models.CharField(max_length=50, choices=COMPETENCE_TYPES,null=True, blank=True)
 
-"""    
-    def __str__(self):
-        return self.user.username
+
+def __str__(self):
+    return self.user.username
+
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
-    
+    """
     Create or update the user profile
-
+    """
     if created:
-        Member.objects.create(user=instance)
-
-    instance.member.save()
-
-class WishClass(models.Model):
-    
-    A model that keeps track of users classes join or PT sessions.
-   
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    type_class = models.ManyToManyField(Product, blank=True)
- """
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
