906d7d58287222795bfcc1f4d3e8f4422413efaf
Samrawit Tekheste <samrawit.samuel.tk@gmail.com>
2024-06-30 23:20:58 +0000

    Add session model to the fit_app for booking

--- fit_app/models.py
+++ fit_app/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+TYPES = (
+    ('pt', 'Pt'),
+    ('cardio', 'Cardio'),
+    ('strength', 'Strength'),
+    ('stretch', 'Stretch'),
+    ('core', 'Core'),
+    )
 
+class Session(models.Model):
+    
+    title = models.CharField(max_length=200)
+    instructor_name = models.CharField(max_length=200)
+    session_type = models.CharField(choices=TYPES,default='Pt')
--- member_app/models.py
+++ member_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.text import slugify
 
 
 COMPETENCE_TYPES = (
@@ -23,19 +24,26 @@ class Member(models.Model):
     maintaining sucess/fail story.
     """
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image_alt = models.URLField(blank=True)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    image_alt = models.URLField(blank=True, null=True)
+    first_name = models.CharField(max_length=30, null=True)
+    last_name = models.CharField(max_length=30, null=True)
     date_of_birth = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=10, choices=GENDER, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     city = models.CharField(max_length=40, null=True, blank=True)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
     slug = models.SlugField(max_length=200, unique=True)
     competence_types = models.CharField(max_length=50, choices=COMPETENCE_TYPES,null=True, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
+def save(self, *args, **kwargs):
+    if not self.slug:
+        self.slug = slugify(f"{self.user.username}-{self.first_name}-{self.last_name}")
+    super(Member, self).save(*args, **kwargs)
 
 def __str__(self):
-    return self.user.username
+    return f"{self.first_name} {self.last_name} ({self.user.username})"
 
 
 @receiver(post_save, sender=User)
